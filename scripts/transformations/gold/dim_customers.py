f161f4711386d62d63d9ddfdd506c14170f748be
Rahul-79 <110174175+Rahul-79@users.noreply.github.com>
2025-10-29 17:21:01 +0530

    Create dim_customers.py

---
+++ scripts/transformations/gold/dim_customers.py
@@ -0,0 +1,13 @@
+import dlt
+
+dlt.create_streaming_table(
+    name = 'dim_customers'
+)
+
+dlt.create_auto_cdc_flow(
+    target = "dim_customers",
+    source = "customers_enr_view",
+    keys = ["customer_id"],
+    sequence_by = 'last_updated',
+    stored_as_scd_type = 2
+)
