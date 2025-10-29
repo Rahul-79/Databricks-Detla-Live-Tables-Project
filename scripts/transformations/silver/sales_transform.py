3ceb6ccc4d52fbedd24f0cd5985852dc12f9d48f
Rahul-79 <110174175+Rahul-79@users.noreply.github.com>
2025-10-29 17:16:00 +0530

    Create sales_transform.py

---
+++ scripts/transformations/silver/sales_transform.py
@@ -0,0 +1,24 @@
+import dlt
+from pyspark.sql.functions import *
+
+
+@dlt.view(
+    name = 'sales_enr_view'
+)
+def sales_stg_transform():
+    df = spark.readStream.table('sales_stg')
+    df = df.withColumn('total_amount',col('quantity') * col('amount'))
+    return df
+
+
+dlt.create_streaming_table(
+    name = 'sales_enriched'
+)
+dlt.create_auto_cdc_flow(
+    target = "sales_enriched",
+    source = "sales_enr_view",
+    keys = ["sales_id"],
+    sequence_by = 'sale_timestamp',
+    stored_as_scd_type = 1
+)
+
